63c173c32bd6a3fff53e27a7960ad2ad5e410f86
David Fekke <dfekke@yourmsc.com>
2013-09-06 11:05:11 -0400

    Added Bootstrap and connect disconnect functionality

--- JaxDugChat/App_Start/BundleConfig.cs
+++ JaxDugChat/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace JaxDugChat
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap/bootstrap.css", 
+                "~/Content/bootstrap/bootstrap-theme.css"));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
